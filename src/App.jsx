4608653ea631a052bc756fa05cacf0b5c6bd54ef
abdelmalek <abdelmalekmaskri18@gmail.com>
2024-07-03 01:41:35 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import HomePage from './pages/HomePage';
+import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom'
+import MainLayout from './layouts/MainLayout';
+import JobsPage from './pages/JobsPage';
+import NotFoundPage from './pages/NotFoundPage';
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path='' element={<MainLayout />}>
+    <Route index element={<HomePage />}/>
+    <Route path='/jobs' element={<JobsPage />}/>
+    <Route path='/*' element={<NotFoundPage />}/>
+  </Route>
+  ));
+const App = () => {
+  return <RouterProvider router={router}/>
+    
+  
+}
+
+export default App
