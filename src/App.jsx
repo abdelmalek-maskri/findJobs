d4ff9d047bda9620527b327ef0935cd37884f00c
abdelmalek <abdelmalekmaskri18@gmail.com>
2024-07-03 23:06:08 +0100

    added a page for each job using useEffect and useParams hooks

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} fr
 import MainLayout from './layouts/MainLayout';
 import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
+import JobPage from './pages/JobPage';
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path='' element={<MainLayout />}>
     <Route index element={<HomePage />}/>
     <Route path='/jobs' element={<JobsPage />}/>
+    <Route path='/jobs/:id' element={<JobPage />}/>
     <Route path='/*' element={<NotFoundPage />}/>
   </Route>
   ));
